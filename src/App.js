73749b55b8d5d83939a0aac0e550a8ff0326bea1
Rashid Akhtar <akhtar.rashid93@gmail.com>
2023-11-20 20:31:01 +0530

    404 errors and gitignore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TodoContainerFunction from './todo-components/TodoContainerFunction';
 import About from './about/About';
 import Counter from './components/Counter';
+import NotFound from './NotFoundPage/NotFound';
 
 
 const id = 1;
@@ -12,7 +13,7 @@ function App() {
       <div className="App">
         <ul className="app-header">
           <li>
-            <Link to={`/${id}`}>Task List</Link>
+            <Link to={`/tasklist/${id}`}>Task List</Link>
           </li>
           <li>
             <Link to="/counter">Counter App</Link>
@@ -22,9 +23,10 @@ function App() {
           </li>
         </ul>
         <Routes>
-          <Route exact path='/:id' element={ <TodoContainerFunction /> }></Route>
+          <Route exact path='/tasklist/:id' element={ <TodoContainerFunction /> }></Route>
           <Route exact path='/counter' element={ <Counter/> }></Route>
           <Route exact path='/about' element={ <About/> }></Route>
+          <Route path="*" element={ <NotFound /> }></Route>
         </Routes>
       </div>
     </Router>
---
+++ src/NotFoundPage/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <h1>404! The page is not found</h1>
+  )
+}
+export default NotFound;
